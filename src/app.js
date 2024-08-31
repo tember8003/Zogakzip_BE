c68a686a604bcf1f9a58101d3dedf3007d546d35
yuchan <yuchan8003@naver.com>
2024-09-01 06:40:16 +0900

    Feat: Image Url 생성 logic 추가

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ dotenv.config();
 import express from 'express';
 
 import groupController from './controllers/groupController.js';
+import ImageController from './controllers/ImageController.js';
 import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 app.use(express.json());
-
+app.use('/uploads', express.static('uploads'));
 app.use('/api/groups', groupController);
+app.use('/api/image', ImageController);
 
 app.use(errorHandler);
 
